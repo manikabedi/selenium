01cca01311ddd28a98df03ede1c9579cc00caf61
manikabedi <manikabedi@yahoo.com>
2019-08-11 19:33:08 +0530

    Second commit

--- src/main/java/projectauto/selenium/App.java
+++ src/main/java/projectauto/selenium/App.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class App 
 {
 	public static void main(String[] args) throws InterruptedException {
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\manikabedi\\Desktop\\selenium\\chromedriver_win32\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Manika Bedi\\eclipse-workspace\\seleniumproject\\selenium\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		
 		driver.get("http://www.gmail.com");
